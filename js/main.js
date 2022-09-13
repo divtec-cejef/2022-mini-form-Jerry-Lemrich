30606ce4592ea369e1f559e6e9b9d9183ea95bdd
Jerry-Lemrich <lemrichjerry@gmail.com>
2022-09-13 11:05:02 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 /**
  * @author Steve Fallet
  * @since 2022.09.06
@@ -5,4 +6,16 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+// A l'envoi du formulaire
+const formulaire = document.querySelector('form');
+formulaire.addEventListener('submit', function(event){
+    event.preventDefault(); // Annule l'envoi du formulaire
+    alert('Envoyé !');
+});
+
+// Au changement de la valeur de la liste
+const listeBackground = document.getElementById('background');
+listeBackground.addEventListener('change', function(){
+    const image = `./img/backgrounds/${listeBackground.value}`
+    document.body.style.backgroundImage = `url(${image})`
+});
