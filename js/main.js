4c5c2f988d3fe064e9023657abe6ae64188e57f7
Jerry-Lemrich <lemrichjerry@gmail.com>
2022-09-20 08:50:25 +0200

    animation

--- js/main.js
+++ js/main.js
@@ -6,11 +6,20 @@
 
 'use strict';
 
+//récuper le bouton
+const btCreer = document.querySelector('[type = "submit"]');
+btCreer.classList.add('animate__heartBeat');
+btCreer.addEventListener('animationend',function(){
+    alert('Envoyé !');
+    formulaire.reset();
+
+});
+
 // A l'envoi du formulaire
 const formulaire = document.querySelector('form');
 formulaire.addEventListener('submit', function(event){
     event.preventDefault(); // Annule l'envoi du formulaire
-    alert('Envoyé !');
+    btCreer.classList.remove('animate__heartBeat');
 });
 
 // Au changement de la valeur de la liste
